3348f4f64eb11a6b2e4086901f565bcb2485bd98
rosshpayne <rosshpayne@gmail.com>
2021-10-30 00:11:20 +0000

    more debug

--- gql/ast/output.go
+++ gql/ast/output.go
@@ -108,7 +108,10 @@ func (r *RootStmt) MarshalJSON() string {
 
 							switch z := scalar.Value.(type) {
 							case [][]string:
-								fmt.Printf("%s%s: %d %d,\n", strings.Repeat("\t", 2), pred, len(z[i]), len(z[i][j]))
+								fmt.Printf("%s%s: i %d j %d %d %d,\n", strings.Repeat("\t", 2), pred, i, j, len(z[i]), len(z[i][j]))
+								if len(z[i][j]) > 0 {
+									fmt.Sprintf("%s%s: %q,\n", strings.Repeat("\t", 2), pred, z[i][j])
+								}
 								s.WriteString(fmt.Sprintf("%s%s: %q,\n", strings.Repeat("\t", 2), pred, z[i][j]))
 							case [][]int64:
 								s.WriteString(fmt.Sprintf("%s%s: %d,\n", strings.Repeat("\t", 2), pred, z[i][j]))
